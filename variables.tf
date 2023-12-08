650b417e6e6664ead5a7ac138f3da16130c7f8ad
Gary Benson <gary@gbenson.net>
2023-12-08 15:59:18 +0000

    terraform import aws_security_group.ssh_admin

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "admin_ip_prefix" {
+  description = "CIDR from which administration may be performed"
+  type        = string
+  sensitive   = true
+}
+
 variable "admin_ssh_key" {
   description = "SSH public key for server admin"
   type        = string
